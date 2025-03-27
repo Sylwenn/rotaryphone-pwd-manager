fd47c75d6becb942138a8156c9315e5b289d3e6f
Syl <35732810+Sylwenn@users.noreply.github.com>
2025-03-27 11:49:25 +0300

    actual init
    lel

---
+++ rotarypwdmngr/includes.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <windows.h>
+#include <string>
+
+
+//Math
+#include <vector>
+#include <chrono>
+#include <random>
+#include <algorithm>
+#include <cmath>
+
+//IMGUI (TODO)
