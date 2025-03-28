22cbdb7620be6cf1a503098dd4ec97ad938397a0
Syl <35732810+Sylwenn@users.noreply.github.com>
2025-03-28 12:26:34 +0300

    whoops updated gitignore (also fixed includes don't add non lib stuff in linker input)

--- rotarypwdmngr/rotarypwdmngr.cpp
+++ rotarypwdmngr/rotarypwdmngr.cpp
@@ -13,7 +13,7 @@ bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
 void CreateRenderTarget();
 void CleanupRenderTarget();
-LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);	
 string HandleWebsiteClick(int index, std::vector<std::string> passwordelements)
 {
 	selectedIndex = index;
